5be5afe91f7527dff084f71d3aa32812273cb001
Lucia Paredes <lparedes15@isc.lp.upb.edu>
2019-12-10 03:01:50 -0400

    button Login solved string problems

--- app/src/main/java/edu/upb/travesia/ui/activities/LoginActivity.java
+++ app/src/main/java/edu/upb/travesia/ui/activities/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,8 +24,8 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        TextView login_text = findViewById(R.id.login_text);
-        login_text.setOnClickListener(new View.OnClickListener() {
+        Button buttonLogin = findViewById(R.id.loginButton);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(context, MainActivity.class);
