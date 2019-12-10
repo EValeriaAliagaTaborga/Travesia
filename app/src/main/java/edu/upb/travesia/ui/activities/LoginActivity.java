d62bc2c41f883d24e71b4d490df2c9b65baff319
EValeriaAliagaTaborga <valealiaga2121@gmail.com>
2019-12-10 03:10:13 -0400

    Splash Activity Done. Login button fixed and other String bugs fixed.

--- app/src/main/java/edu/upb/travesia/ui/activities/LoginActivity.java
+++ app/src/main/java/edu/upb/travesia/ui/activities/LoginActivity.java
@@ -14,21 +14,22 @@ import edu.upb.travesia.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Button btnLogin;
+
     private static final String LOG = "LoginActivity";
     private Context context;
 
     protected void onCreate(Bundle savedInstanceState) {
 
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button buttonLogin = findViewById(R.id.loginButton);
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
+
+        btnLogin = findViewById(R.id.loginButton);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(context, MainActivity.class);
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
